d8ee84620fbc8ee4868d0ee5127d0743676ebc99
Addison <addison@framer.com>
2019-05-24 15:25:26 +0200

    staging changes from circle jsx

--- design-system/index.tsx
+++ design-system/index.tsx
@@ -1,4 +1,6 @@
-export { Button, Props as ButtonProps } from "./components/Button"
-export { Input, Props as InputProps } from "./components/Input"
-export { Toggle, Props as ToggleProps } from "./components/Toggle"
-export { Tooltip, Props as TooltipProps } from "./components/Tooltip"
+export { Button, Props as ButtonProps } from "./components/Button";
+export { ButtonCopy } from "./components/Button Copy";
+export { Input, Props as InputProps } from "./components/Input";
+export { Toggle, Props as ToggleProps } from "./components/Toggle";
+export { Tooltip, Props as TooltipProps } from "./components/Tooltip";
+export { Circle } from "./components/Circle";

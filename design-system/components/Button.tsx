4d8c53c793ab9d8015f61a8d568798d20af14a2b
Addison <addison@framer.com>
2019-06-05 18:49:00 +0200

    changed border radius

--- design-system/components/Button.tsx
+++ design-system/components/Button.tsx
@@ -37,7 +37,7 @@ export const Button: React.FC<Props> = ({
 // Styles
 const StyledButton = styled.button`
   background: ${theme.color.paneBg};
-  border-radius: 4px;
+  border-radius: 14px;
   border: none;
   box-shadow: inset 0 0 0 1px rgba(0, 0, 0, 0.1);
   color: ${theme.color.body};
